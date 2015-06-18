9dc475aff0fbdb14ae26d02dbb1a250a48eafb66
Michał Budzyński <michal@virtualdesign.pl>
2015-06-19 00:47:15 +0200

    :atm: working treewalking

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
   var walk = require('./utils/walker.js');
   var dir = process.cwd();
 
-  //console.log(walk);
-  walk(dir, function(result){
+  console.log(dir);
+  walk(dir, function(err, result){
     console.log(result);
   });
 
