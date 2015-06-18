186a20a4e123a8fb889a211d492f56d8743cbe14
Michał Budzyński <michal@virtualdesign.pl>
2015-06-18 01:01:00 +0100

    :hearts: use new walker schema

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
   'use strict';
   var tree = require('./walker');
   var dir = process.cwd();
-  var output = '';
 
-  var parseElement = function(element) {
-
-  };
   tree(dir, function(err, result){
-
+    console.log(result);
   });
 
 })();
