9f95b399656ffea1eee840fe44bbe2ce393ed433
Michał Budzyński <michal@virtualdesign.pl>
2015-06-18 01:01:00 +0100

    :lipstick: indentation counter

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
   'use strict';
   var tree = require('./walker');
   var dir = process.cwd();
+  var indentation = 0;
 
   var output = '';
+
+  var addIndentation = function(){
+    return new Array((indentation*2)+1).join(' ');
+  };
+
   var parseResult = function(result){
+    indentation++;
     for (var i in result) {
       if (typeof result[i] === 'string') {
-        output += '[' + result[i] + '](' + i + ')\n';
+        output += addIndentation() + '[' + result[i] + '](' + i + ')\n';
       } else if (typeof result[i] === 'object'){
-        output += 'folder: ' + i + '\n';
+        output += addIndentation() + 'folder: ' + i + '\n';
         parseResult(result[i]);
+        indentation--;
       }
     }
   };
