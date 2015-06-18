2ed4b5476d827d570cd5c3ef4aad10b94c361156
Michał Budzyński <michal@virtualdesign.pl>
2015-06-18 01:01:00 +0100

    :heart_eyes: create md links from files

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
   var tree = require('./walker');
   var dir = process.cwd();
 
+  var output = '';
+  var parseResult = function(result){
+    for (var i in result) {
+      if (typeof result[i] === 'string') {
+        output += '[' + result[i] + '](' + i + ')\n';
+      } else if (typeof result[i] === 'object'){
+        parseResult(result[i]);
+      }
+    }
+  };
   tree(dir, function(err, result){
-    console.log(result);
+    parseResult(result);
+    console.log(output);
   });
 
 })();
