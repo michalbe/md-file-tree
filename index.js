c285211702abccad96340119c750c20bd3b9c387
MattIPv4 <matthew@cowley.org.uk>
2019-08-25 11:23:53 +0100

    ♻️ move require to top, use String.repeat instead of Array.join

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
+const tree = require('./walker');
+
 const cleanMarkdown = name => name.replace(/([\\\/_*|-])/g, '\\$1');
 const directoryName = name => {
     return '- __' + cleanMarkdown(name) + '__\n';
@@ -9,18 +11,16 @@ const filename = (name, path) => {
     const link = path.replace(/^\/?(.+?)\/?$/, '$1') + '/' + encodeURIComponent(name);
     return '- [' + cleanMarkdown(name) + '](' + link + ')\n';
 };
+const addIndentation = i => {
+    return ' '.repeat(i * 2 + 1);
+};
 
 const main = () => {
-    const tree = require('./walker');
     const dirPath = process.cwd();
     const dir = dirPath.split('/').pop();
+
     let indentation = 0;
-
     let output = directoryName(dir);
-
-    const addIndentation = () => {
-        return new Array((indentation * 2) + 1).join(' ');
-    };
 
     const parseResult = result => {
         indentation++;
@@ -28,14 +28,15 @@ const main = () => {
             const data = result[key];
             if (typeof data === 'string' && key[0] !== '.') {
                 const path = data.split('/');
-                output += addIndentation() + filename(path.pop(), path.join('/'));
+                output += addIndentation(indentation) + filename(path.pop(), path.join('/'));
             } else if (typeof data === 'object') {
-                output += addIndentation() + directoryName(key);
+                output += addIndentation(indentation) + directoryName(key);
                 parseResult(data);
                 indentation--;
             }
         });
     };
+
     tree(dirPath, (err, result) => {
         parseResult(result);
         console.log(output);
